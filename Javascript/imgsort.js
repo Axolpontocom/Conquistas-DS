8396af9a2d87554c503d9fbbda3b71c36742c7c0
Axolpontocom <jarturparanhos@gmail.com>
2026-08-16 01:57:29 -0300

    move a definição do array sortimagem para dentro do evento DOMContentLoaded

--- Javascript/imgsort.js
+++ Javascript/imgsort.js
@@ -1,14 +1,17 @@
-const sortimagem = [
-    "../img/goatsouls.jfif",
-    "../img/undead.gif",
-    "../img/Carson-Drew-It_com_ Photo.jfif",
-    "../img/dsfeio.jfif",
-    "../img/plimplimplom.gif",
-    "../img/elden-ring-vagabond.webp",
-];
+document.addEventListener("DOMContentLoaded", function() {
+    const sortimagem = [
+        "../img/goatsouls.jfif",
+        "../img/undead.gif",
+        "../img/Carson-Drew-It_com_ Photo.jfif",
+        "../img/dsfeio.jfif",
+        "../img/plimplimplom.gif",
+        "../img/elden-ring-vagabond.webp",
+    ];
 
-const numaleatorio = Math.floor(Math.random() * sortimagem.length);
+    const numaleatorio = Math.floor(Math.random() * sortimagem.length);
+    const img = document.getElementById("img-sort");
 
-const img = document.getElementById("img-sort");
-
-img.src = sortimagem[numaleatorio];
+    if (img) {
+        img.src = sortimagem[numaleatorio];
+    }
+});
