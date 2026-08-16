246badf922bb8e3ece86a655d5180148c8266f5d
Axolpontocom <jarturparanhos@gmail.com>
2026-08-16 01:49:15 -0300

    o sorteio de imagem foi separado dos arquivos DS1-DS3 e do CONQUISTAS

--- Javascript/imgsort.js
+++ Javascript/imgsort.js
@@ -1,17 +1,10 @@
 const sortimagem = [
-    "../../img/goatsouls.jfif",
-    "../../img/undead.gif",
-    "../../img/Carson-Drew-It_com_ Photo.jfif",
-    "../../img/dsfeio.jfif",
-    "../../img/plimplimplom.gif",
-    "../../img/elden-ring-vagabond.webp",
-     "../img/goatsouls.jfif",
+    "../img/goatsouls.jfif",
     "../img/undead.gif",
     "../img/Carson-Drew-It_com_ Photo.jfif",
     "../img/dsfeio.jfif",
     "../img/plimplimplom.gif",
-    "../img/elden-ring-vagabond.webp"
-
+    "../img/elden-ring-vagabond.webp",
 ];
 
 const numaleatorio = Math.floor(Math.random() * sortimagem.length);
---
+++ Javascript/imgsortcon.js
@@ -0,0 +1,14 @@
+const sortimagem = [
+    "../../img/goatsouls.jfif",
+    "../../img/undead.gif",
+    "../../img/Carson-Drew-It_com_ Photo.jfif",
+    "../../img/dsfeio.jfif",
+    "../../img/plimplimplom.gif",
+    "../../img/elden-ring-vagabond.webp",
+];
+
+const numaleatorio = Math.floor(Math.random() * sortimagem.length);
+
+const img = document.getElementById("img-sort");
+
+img.src = sortimagem[numaleatorio];
